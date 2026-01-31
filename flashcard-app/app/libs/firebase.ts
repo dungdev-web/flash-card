e92f0332bc24fadc162cc4b549576b158acb1ed7
LƯU ĐỨC DŨNG <dung.dev.web@gmail.com>
2026-01-31 20:29:37 +0700

    Update firebase.ts

--- flashcard-app/app/libs/firebase.ts
+++ flashcard-app/app/libs/firebase.ts
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaTl_YlwYVKwYDxxByMVTPx9KT4epkevg",
-  authDomain: "fir-b4383.firebaseapp.com",
-  projectId: "fir-b4383",
-  storageBucket: "fir-b4383.firebasestorage.app",
-  messagingSenderId: "691868026276",
-  appId: "1:691868026276:web:bb61db5539657c2d4cad31",
-  measurementId: "G-KYYEHHF2ET"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 const app = initializeApp(firebaseConfig);
 
